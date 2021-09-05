094a3d29947b7ac7f49708465dde62280655e6af
Ban <kevintoptschij12012003@gmail.com>
2021-09-06 01:30:12 +0200

    Added Start-Up Code

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pygame
 from sys import exit
 
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.fill("Red")
+
+
+
